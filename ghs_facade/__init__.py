b4efcabc883e3538ba2e8360f3dd21c8326c3fed
Julius <julius.brehme@googlemail.com>
2024-06-17 19:37:45 +0200

    allow cors for local env

--- ghs_facade/__init__.py
+++ ghs_facade/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 import gitlab
 
 # Initialize the Flask application
 app = Flask(__name__)
+CORS(app)
 
 # Configuration for GitLab
 PERSONAL_ACCESS_TOKEN = os.environ.get('PERSONAL_ACCESS_TOKEN')
