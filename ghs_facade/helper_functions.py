6676607c3f4ee134c5c5876a5a257ca941d15b87
Julius <julius.brehme@googlemail.com>
2024-06-20 14:48:26 +0200

    updated function

--- ghs_facade/helper_functions.py
+++ ghs_facade/helper_functions.py
@@ -1,13 +1,3 @@
-def getProjects(response: list[any]) -> list[any]:
-    projects = []
-    for project in response:
-        simpleProject = {}
-        simpleProject["id"] = project["id"]
-        simpleProject["name"] = project["name"]
-        projects.append(simpleProject)
-    return projects
-
-
 def getProjects(projects) -> list[any]:
     allProjects = []
     for project in projects:
